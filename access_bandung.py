afbe12417efb6e69cc056121ae89f6807fa9cb33
Shulha Jawas <80399465+shulhajws@users.noreply.github.com>
2023-04-17 16:01:35 +0700

    Update access_bandung.py

--- access_bandung.py
+++ access_bandung.py
@@ -1,1 +1,28 @@
-#F03 #F04 #F05 #F08 #F09 
+#F03 #F04 #F5 #F08 #F09 #F10
+
+#F03 Summon Jin
+def summon():
+    kasdf
+
+#F04 Hilangkan Jin
+def hapus():
+    asddf
+
+#F05 Ubah Jin
+def ubah():
+    hkj
+
+#F08 Batch Kumpul
+def batchkumpul():
+    jkl
+
+def batchbangun():
+    adfdaj
+
+#F09 Laporan Jin
+def laporanjin():
+    adf
+
+#F10 Laporan Candi
+def laporancandi():
+    asdf
