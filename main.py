6552b88b8ca9cc2e94dd5dd8ba83c351c36a39d3
Shulha Jawas <80399465+shulhajws@users.noreply.github.com>
2023-04-17 17:26:09 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-#Program Utama
->>> masukannya apa
-    commands(masukannyapa)
+import loadcsv
+import commands
+
+#MATRIKS
+users = loadcsv.loaduser() # Matriks data user
+candi = loadcsv.loadcandi() # Matriks data candi
+bahan_bangunan = loadcsv.loadbahan() # Data bahan bangunan
+
+role="-"
+uname="-"
+
+while True:
+  masukan = input(">>> ")
+  commands.run(masukan,role)
