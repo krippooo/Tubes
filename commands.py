5487533ab135efe41c9690e88cf0bce34d023a57
Shulha Jawas <80399465+shulhajws@users.noreply.github.com>
2023-04-18 22:15:50 +0700

    Update commands.py

--- commands.py
+++ commands.py
@@ -4,87 +4,82 @@ def run(input,role,uname,users,candi,bahan_bangunan):
     #Akses: Semua
     if input=="login":
         uname,role=access_all.login(uname,users,role)
-        return (role,uname,users,candi,bahan_bangunan)
-    if input=="logout":
+    elif input=="logout":
         if role!="-":
             role,uname = access_all.logout(role,uname)
         else:
             print("Anda belum login, silahkan login terlebih dahulu sebelum melakukan logout")
-        return (role,uname,users,candi,bahan_bangunan)
-    if input=="help":
+    elif input=="help":
         access_all.help()
-    if input=="exit":
+    elif input=="exit":
         access_all.exit()
-    if input=="tempstate":
+    elif input=="tempstate":
         access_all.tempstate(role,uname,users,candi,bahan_bangunan)
-        return (role,uname,users,candi,bahan_bangunan)
-
 
     #Akses: Bandung Bondowoso
-    if input=="summonjin" : 
+    elif input=="summonjin" : 
         if role=="bandung_bondowoso":
             users=access_bandung.summon(users)
-            return (role,uname,users,candi,bahan_bangunan)
         else:
             print("Anda tidak dapat melakukannya. Silakan gunakan command 'help' untuk melihat yang dapat Anda lakukan.")
-    if input=="hapusjin":
+    elif input=="hapusjin":
         if role=="bandung_bondowoso":
             access_bandung.hapus()
         else:
             print("Anda tidak dapat melakukannya. Silakan gunakan command 'help' untuk melihat yang dapat Anda lakukan.")
-    if input=="ubahjin":
+    elif input=="ubahjin":
         if role=="bandung_bondowoso":
             access_bandung.ubah()
         else:
             print("Anda tidak dapat melakukannya. Silakan gunakan command 'help' untuk melihat yang dapat Anda lakukan.")
-    if input=="batchkumpul":
+    elif input=="batchkumpul":
         if role=="bandung_bondowoso":
             bahan_bangunan=access_bandung.batchkumpul(bahan_bangunan)
-            return (role,uname,users,candi,bahan_bangunan)
         else:
             print("Anda tidak dapat melakukannya. Silakan gunakan command 'help' untuk melihat yang dapat Anda lakukan.")
-    if input=="batchbangun":
+    elif input=="batchbangun":
         if role=="bandung_bondowoso":
            candi,bahan_bangunan=access_bandung.batchbangun(uname,users,bahan_bangunan)
-           return (role,uname,users,candi,bahan_bangunan)
         else:
             print("Anda tidak dapat melakukannya. Silakan gunakan command 'help' untuk melihat yang dapat Anda lakukan.")
-    if input=="laporanjin":
+    elif input=="laporanjin":
         if role=="bandung_bondowoso":
             access_bandung.laporanjin()
         else:
             print("Anda tidak dapat melakukannya. Silakan gunakan command 'help' untuk melihat yang dapat Anda lakukan.")
-    if input=="laporancandi":
+    elif input=="laporancandi":
         if role=="bandung_bondowoso":
             access_bandung.laporancandi(candi)
-            return (role,uname,users,candi,bahan_bangunan)
         else:
             print("Anda tidak dapat melakukannya. Silakan gunakan command 'help' untuk melihat yang dapat Anda lakukan.")
     
     #Akses: Roro Jonggrang
-    if input=="hancurkancandi":
+    elif input=="hancurkancandi":
         if role=="roro_jonggrang":
             access_roro.hancurkancandi()
         else:
             print("Anda tidak dapat melakukannya. Silakan gunakan command 'help' untuk melihat yang dapat Anda lakukan.")
-    if input=="ayamberkokok":
+    elif input=="ayamberkokok":
         if role=="roro_jonggrang":
             access_roro.ayamberkokok()
         else:
             print("Anda tidak dapat melakukannya. Silakan gunakan command 'help' untuk melihat yang dapat Anda lakukan.")
 
     #Akses: Jin Pengumpul
-    if input=="kumpul":
+    elif input=="kumpul":
         if role=="jin_pengumpul":
             bahan_bangunan=access_jin.kumpul(bahan_bangunan)
-            return (role,uname,users,candi,bahan_bangunan)
         else:
             print("Anda tidak dapat melakukannya. Silakan gunakan command 'help' untuk melihat yang dapat Anda lakukan.")
 
     #Akses: Jin Pembangun
-    if input=="bangun":
+    elif input=="bangun":
         if role=="jin_pembangun":
             candi,bahan_bangunan=access_jin.bangun(uname,users,candi,bahan_bangunan)
-            return (role,uname,users,candi,bahan_bangunan)
         else:
             print("Anda tidak dapat melakukannya. Silakan gunakan command 'help' untuk melihat yang dapat Anda lakukan.")
+
+    else:
+        print("Anda tidak dapat melakukannya. Silakan gunakan command 'help' untuk melihat yang dapat Anda lakukan.")
+
+    return (role,uname,users,candi,bahan_bangunan)
