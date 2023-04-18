208b337dc20a7d8317306aa278a2177393e29f52
Shulha Jawas <80399465+shulhajws@users.noreply.github.com>
2023-04-18 20:35:51 +0700

    Update access_all.py

--- access_all.py
+++ access_all.py
@@ -1,22 +1,38 @@
 #F01 #F02 #F15 #F16
 
 #F01 Login
-def login():
-    uname = input("usr")
-    pw = input("pw")
-    role = "-"
-    with open("user.csv", mode='r') as f:        #buat ngeakses si csv
-        reader = csv.DictReader(f, delimiter=';')                           #ngeread csv
-        for row in reader:                                                  
-            if row['username'] == uname and row['password'] == pw :         #row[blabla] sesuai kolom
-                print("Selamat Datang, "+uname+"!")
-                print("Masukkan Command \"help\" untuk daftar command yang dapat kamu panggil.")
-                role = row['role']
-                return (role)        
-        if row['username'] != uname :
+def login(users,role):
+    if role=="-": #Cek apakah sudah ada pemain yang login
+        #Minta masukan
+        uname = input("Masukkan username: ")
+    
+        #Cek availability username
+        registered=False
+        found=0
+        for i in range(110):
+            if uname==users[i][0]:
+                registered=True
+                found=i
+                break
+        
+        if not registered:
             print("Username tidak terdaftar!")
-        elif row['password'] != pw :
-            print("Password salah!")
+        else: #Jika username registered
+            #Cek password
+            pw = input("Masukkan password: ")
+            if users[found][1]==pw:
+                print(f"Selamat datang, {uname}!")
+                print("Masukkan command “help” untuk daftar command yang dapat kamu panggil.")
+                role=users[found][2]
+            else:
+                print("Password salah!")
+
+    else: #Jika sudah ada pemain yang login
+        print("Login gagal!")
+        print(f"Anda telah login dengan username {uname}, silahkan lakukan “logout” sebelum melakukan login kembali.")
+
+    return(uname,role)
+
 
 #F02 Logout
 def logout(akses,uname):
