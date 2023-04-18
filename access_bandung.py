e768ca6da41afb5c28f99c2ad57b8bbc7d4cf985
Shulha Jawas <80399465+shulhajws@users.noreply.github.com>
2023-04-18 10:47:32 +0700

    Update access_bandung.py
    shulha 18/4 10.47

--- access_bandung.py
+++ access_bandung.py
@@ -12,7 +12,7 @@ def hapus():
 def ubah():
     hkj
 
-#F08 Batch Kumpul
+#F08 Batch Kumpul Bangun
 def batchkumpul():
     jkl
 
@@ -24,5 +24,46 @@ def laporanjin():
     adf
 
 #F10 Laporan Candi
-def laporancandi():
-    asdf
+def laporancandi(candi):
+    #Jumlah Candi
+    from access_jin import hitungcandi
+    jumlah_candi=hitungcandi(candi)
+    
+    #Total Bahan
+    total_pasir = 0
+    for i in range (1, 110):
+        if candi[i][2]!="":
+            total_pasir+=int(candi[i][2])
+    total_batu = 0
+    for i in range (1, 110):
+        if candi[i][3]!="":
+            total_batu+=int(candi[i][3])
+    total_air = 0
+    for i in range (1, 110):
+        if candi[i][4]!="":
+            total_pasir+=int(candi[i][4])
+
+    #Harga Candi
+    maks=0
+    mins=200000
+    for i in range(1, 110):
+        if candi[i][0]!="":
+            harga_candi = 10000 * int(candi[i][2]) + 15000 * int(candi[i][3]) + 7500 * int(candi[i][4])
+            if harga_candi>maks:
+                maks=harga_candi
+                id_termahal= candi[i][0]
+            if harga_candi<mins:
+                mins=harga_candi
+                id_termurah= candi[i][0]
+
+    #Output
+    print(f"Total candi: {jumlah_candi}")
+    print(f"Total Pasir yang digunakan: {total_pasir}")
+    print(f"Total Batu yang digunakan: {total_batu}")
+    print(f"Total Air yang digunakan: {total_air}")
+    print(f"ID Candi Termahal: {id_termahal} ({maks})")
+    print(f"ID Candi Termurah:  {id_termurah} ({mins})")
+
+    
+
+        
