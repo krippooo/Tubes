57778bf5c892273649cbeb0c031c4a10f27cf6d9
Shulha Jawas <80399465+shulhajws@users.noreply.github.com>
2023-04-18 08:59:16 +0700

    Update main.py
    shulha 18/4 9.00

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ import loadcsv
 import commands
 
 #MATRIKS
+users=[]
+candi=[]
+bahan_bangunan=[]
 users = loadcsv.loaduser() # Matriks data user
 candi = loadcsv.loadcandi() # Matriks data candi
 bahan_bangunan = loadcsv.loadbahan() # Data bahan bangunan
 
-role="-"
-uname="-"
+role="abc"
+uname="abc"
 
 while True:
   masukan = input(">>> ")
-  commands.run(masukan,role)
+  role,uname,users,candi,bahan_bangunan=commands.run(masukan,role,uname,users,candi,bahan_bangunan)
+  
