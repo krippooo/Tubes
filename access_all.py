dfaf58b44a54df044fe4dfeaf554adc08c6bf765
krippooo <116004310+krippooo@users.noreply.github.com>
2023-04-18 15:04:06 +0700

    Update access_all.py
    F01 login uda

--- access_all.py
+++ access_all.py
@@ -2,7 +2,21 @@
 
 #F01 Login
 def login():
-    asdfg
+    uname = input("usr")
+    pw = input("pw")
+    role = "-"
+    with open(r"C:\Tubes daspro\csv tubes\user.csv", mode='r') as f:        #buat ngeakses si csv
+        reader = csv.DictReader(f, delimiter=';')                           #ngeread csv
+        for row in reader:                                                  
+            if row['username'] == uname and row['password'] == pw :         #row[blabla] sesuai kolom
+                print("Selamat Datang, "+uname+"!")
+                print("Masukkan Command \"help\" untuk daftar command yang dapat kamu panggil.")
+                role = row['role']
+                return (role)        
+        if row['username'] != uname :
+            print("Username tidak terdaftar!")
+        elif row['password'] != pw :
+            print("Password salah!")
 
 #F02 Logout
 def logout(akses,uname):
