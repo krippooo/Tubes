d7386c94cc95efc3fcdd87f6fc68018fd58710da
wNSYA <114148615+wNSYA@users.noreply.github.com>
2023-04-18 12:19:39 +0700

    Update access_roro.py

--- access_roro.py
+++ access_roro.py
@@ -6,4 +6,30 @@ def hancurkancandi():
 
 #F12 Ayam Berkokok
 def ayamberkokok():
-    asdf
+    from main import candi
+    def panjanglist(arr):
+        cek = ['','','','']
+        count = 0
+        if arr[-1] != cek:
+            while arr[count] != cek:
+                cek = arr[count]
+                count+=1
+            return count-2
+        else:
+            return 100
+    f = open('candi.csv','r')
+    total = panjanglist(candi)
+    if total < 100:
+        print("Kukuruyuk.. Kukuruyuk..\n")
+        print(f"Jumlah Candi: {total}\n")
+        print("Selamat, Roro Jonggrang memenangkan permainan!\n\n*Bandung Bondowoso angry noise*\nRoro Jonggrang dikutuk menjadi candi.")
+        exit()
+    else:
+        print("Kukuruyuk.. Kukuruyuk..\n")
+        print(f"Jumlah Candi: {total}\n")
+        print("Yah, Bandung Bondowoso memenangkan permainan!")
+        exit()
+
+
+
+
