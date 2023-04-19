bda0d60607d710161e410d07e367c0115ed09dca
Shulha Jawas <80399465+shulhajws@users.noreply.github.com>
2023-04-19 12:15:54 +0700

    Update commands.py

--- commands.py
+++ commands.py
@@ -51,7 +51,7 @@ def run(input,role,uname,users,candi,bahan_bangunan):
             print("Anda tidak dapat melakukannya. Silakan gunakan command 'help' untuk melihat yang dapat Anda lakukan.")
     elif input=="laporanjin":
         if role=="bandung_bondowoso":
-            access_bandung.laporanjin()
+            access_bandung.laporanjin(users,candi,bahan_bangunan)
         else:
             print("Anda tidak dapat melakukannya. Silakan gunakan command 'help' untuk melihat yang dapat Anda lakukan.")
     elif input=="laporancandi":
