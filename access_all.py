93887d453a9fd55d20f1942a50b5afd6b025eed6
krippooo <116004310+krippooo@users.noreply.github.com>
2023-04-26 21:45:07 +0700

    Update access_all.py

--- access_all.py
+++ access_all.py
@@ -57,8 +57,51 @@ def tempstate(role, uname,users,candi,bahan_bangunan):
 
 
 #F15 Help
-def help():
-    asdf
+def help(role):
+    if role == "-" :
+        print("=========== HELP ===========")
+        print("1. login")
+        print("Untuk masuk menggunakan akun")
+        print("2. exit")
+        print("Untuk keluar dari program dan kembali ke terminal")
+    elif role == "bandung_bondowoso":
+        print("=========== HELP ===========")
+        print("1. logout")
+        print("Untuk keluar dari akun yang digunakan sekarang")
+        print("2. summonjin")
+        print("Untuk memanggil jin")
+        print("3. hapusjin")
+        print("Untuk menghapus jin")
+        print('4. ubahjin')
+        print('Untuk mengubah role jin')
+        print('5. batchkumpul')
+        print('Untuk menyuruh para jin pengumpul mengumpulkan bahan')
+        print('6. batchbangun')
+        print('Untuk menyuruh para jin pembangun untuk membangun candi')
+        print('7. laporanjin')
+        print('Untuk melihat kinerja para jin dan informasi mengenai bahan bangunan')
+        print('8. laporancandi')
+        print('Untuk mengetahui progress pembangunan candi')
+    elif role == "roro_jonggrang":
+        print("=========== HELP ===========")
+        print("1. logout")
+        print("Untuk keluar dari akun yang digunakan sekarang")
+        print("2. hancurkancandi")
+        print("Untuk menghancurkan candi dan menggagalkan usaha Bandung Bondowoso")
+        print("3. ayamberkokok")
+        print("Untuk menyelesaikan permainan")
+    elif role == "jin_pembangun":
+        print("=========== HELP ===========")
+        print("1. logout")
+        print("Untuk keluar dari akun yang digunakan sekarang")
+        print('2. bangun')
+        print('Untuk membangun candi')
+    elif role == 'jin_pengumpul':
+        print("=========== HELP ===========")
+        print("1. logout")
+        print("Untuk keluar dari akun yang digunakan sekarang")
+        print("2. kumpul")
+        print('Untuk mengumpulkan bahan bangunan')
 
 #F16 Exit
 def exit():
