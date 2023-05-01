291a49cea0aa00ec367aa23793c1a61b28816403
wNSYA <114148615+wNSYA@users.noreply.github.com>
2023-05-01 20:09:42 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
-import loadcsv
 import commands
-
+import argparse
+import loadsave
+import sys
 #MATRIKS
 users=[]
 candi=[]
 bahan_bangunan=[]
-users = loadcsv.loaduser() # Matriks data user
-candi = loadcsv.loadcandi() # Matriks data candi
-bahan_bangunan = loadcsv.loadbahan() # Data bahan bangunan
 
 role="-"
 uname="-"
+
+parser = argparse.ArgumentParser()
+parser.add_argument('nama_folder',nargs='?', default='__no_value__')
+args = parser.parse_args()
+
+if args.nama_folder == '__no_value__':
+    sys.exit("\nTidak ada nama folder yang diberikan!\n\nUsage: python main.py <nama_folder>.")
+else:
+    users,candi,bahan_bangunan = loadsave.load(args.nama_folder)
+    print("Selamat datang di Program \"Manajerial Candi\"! ")
+    print("masukkan menu login dan silahkan isi username anda.")
 
 while True:
   masukan = input(">>> ")
